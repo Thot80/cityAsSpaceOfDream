f996e677c6f6aed95ed338acf243299816538666
Jeremy Kessous <jeremykessous@yahoo.fr>
2021-11-26 20:48:15 +0100

    ressearch folder toggle pop ups for desktop and modals for smartphone

--- assets/js/script.js
+++ assets/js/script.js
@@ -79,3 +79,62 @@ else{
 $("#calendar").click(function(){
     $("#calendar-text").toggleClass("calendarTextDisabled");
 })
+
+// ------------------------------------ Ressearch folder pop up -----------------------------------------//
+
+ let ressearchModal = new bootstrap.Modal($("#ressearchModal1"));
+
+// ------------------ Desktop behaviour --------------------------//
+
+if(smartPhoneDevice === false){
+
+    // Toggle Hidden ressearch files when folder clicked
+    $("#ressearch-folder").click(function() {
+
+        $(".ressearch-cards").toggleClass("ressearch-active");
+    });
+    
+
+    // Card changing position when clicked
+    $(".ressearch-cards").click( function() {
+    
+        // z-index value match the id of the car, z-index:n -> nth card
+        let clickedIndex = $(this).css("z-index");
+        let clickedCard = $(this);
+        let cardToChange = $("div.third-ressearch-card");
+        if(clickedIndex == 1){
+    
+            clickedCard.removeClass("first-ressearch-card");
+            clickedCard.addClass("third-ressearch-card");
+            cardToChange.removeClass("third-ressearch-card");
+            cardToChange.addClass("first-ressearch-card");
+        }
+        if(clickedIndex == 2){
+    
+            clickedCard.removeClass("second-ressearch-card");
+            clickedCard.addClass("third-ressearch-card");
+            cardToChange.removeClass("third-ressearch-card");
+            cardToChange.addClass("second-ressearch-card");
+        }
+        if(clickedIndex == 3){
+    
+            clickedCard.removeClass("third-ressearch-card");
+            clickedCard.addClass("third-ressearch-card");
+            cardToChange.removeClass("fifth-ressearch-card");
+            cardToChange.addClass("third-ressearch-card");
+        }
+        if(clickedIndex == 4){
+    
+            clickedCard.removeClass("fourth-ressearch-card");
+            clickedCard.addClass("third-ressearch-card");
+            cardToChange.removeClass("fifth-ressearch-card");
+            cardToChange.addClass("third-ressearch-card");
+        }
+    })  
+}
+// Modal Trigger on smartphone
+else{
+    $("#ressearch-folder").click(() => {
+        ressearchModal.show();
+    });
+}
