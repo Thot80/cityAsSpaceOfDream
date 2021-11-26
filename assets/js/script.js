c8cb83fa8f67b5259d54eb701724eda6104c4c4a
Jeremy Kessous <jeremykessous@yahoo.fr>
2021-11-26 18:26:33 +0100

    desktop archieves pop up functionnality

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,60 @@
-// Mooving Banner
+// Getting device screen width
 
+let screenWidth = window.innerWidth;
+let smartPhoneDevice;
+
+// Desktop behaviour
+if (screenWidth > 768){
+    smartPhoneDevice = false;
+}
+// Smartphone behaviour
+else{
+    smartPhoneDevice = true;
+}
+
+
+
+// ---------------- Archieves folder Desktop pop-up ------------------//
+
+$("#archives-folder").click(() => {
+
+    $(".archieves-cards").toggleClass("archieves-active");
+});
+
+
+
+$(".archieves-cards").click( function() {
+
+    // z-index value match the id of the car, z-index:n -> nth card
+    let clickedIndex = $(this).css("z-index");
+    let clickedCard = $(this);
+    let cardToChange = $("div.fifth-archieves-card");
+    if(clickedIndex == 1){
+
+        clickedCard.removeClass("first-archieves-card");
+        clickedCard.addClass("fifth-archieves-card");
+        cardToChange.removeClass("fifth-archieves-card");
+        cardToChange.addClass("first-archieves-card");
+    }
+    if(clickedIndex == 2){
+
+        clickedCard.removeClass("second-archieves-card");
+        clickedCard.addClass("fifth-archieves-card");
+        cardToChange.removeClass("fifth-archieves-card");
+        cardToChange.addClass("second-archieves-card");
+    }
+    if(clickedIndex == 3){
+
+        clickedCard.removeClass("third-archieves-card");
+        clickedCard.addClass("fifth-archieves-card");
+        cardToChange.removeClass("fifth-archieves-card");
+        cardToChange.addClass("third-archieves-card");
+    }
+    if(clickedIndex == 4){
+
+        clickedCard.removeClass("fourth-archieves-card");
+        clickedCard.addClass("fifth-archieves-card");
+        cardToChange.removeClass("fifth-archieves-card");
+        cardToChange.addClass("fourth-archieves-card");
+    }
+})
