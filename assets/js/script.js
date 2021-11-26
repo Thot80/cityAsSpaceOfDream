87399d2afd993fa185669e65365e9315798c6320
Jeremy Kessous <jeremykessous@yahoo.fr>
2021-11-26 19:23:10 +0100

    implementation of archives component for smartphone - Modals

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,7 +4,7 @@ let screenWidth = window.innerWidth;
 let smartPhoneDevice;
 
 // Desktop behaviour
-if (screenWidth > 768){
+if (screenWidth >= 768){
     smartPhoneDevice = false;
 }
 // Smartphone behaviour
@@ -14,47 +14,64 @@ else{
 
 
 
-// ---------------- Archieves folder Desktop pop-up ------------------//
+// ---------------- Archieves folder pop-up ------------------//
 
-$("#archives-folder").click(() => {
+let archievesModal = new bootstrap.Modal($("#archievesModal2021"));
 
-    $(".archieves-cards").toggleClass("archieves-active");
-});
+// ------------------ Desktop behaviour --------------------------//
 
+if(smartPhoneDevice === false){
 
+    // Toggle Hidden archieves files when folder clicked
+    $("#archives-folder").click(() => {
 
-$(".archieves-cards").click( function() {
+        $(".archieves-cards").toggleClass("archieves-active");
+    });
+    
 
-    // z-index value match the id of the car, z-index:n -> nth card
-    let clickedIndex = $(this).css("z-index");
-    let clickedCard = $(this);
-    let cardToChange = $("div.fifth-archieves-card");
-    if(clickedIndex == 1){
+    // Card changing position when clicked
+    $(".archieves-cards").click( function() {
+    
+        // z-index value match the id of the car, z-index:n -> nth card
+        let clickedIndex = $(this).css("z-index");
+        let clickedCard = $(this);
+        let cardToChange = $("div.fifth-archieves-card");
+        if(clickedIndex == 1){
+    
+            clickedCard.removeClass("first-archieves-card");
+            clickedCard.addClass("fifth-archieves-card");
+            cardToChange.removeClass("fifth-archieves-card");
+            cardToChange.addClass("first-archieves-card");
+        }
+        if(clickedIndex == 2){
+    
+            clickedCard.removeClass("second-archieves-card");
+            clickedCard.addClass("fifth-archieves-card");
+            cardToChange.removeClass("fifth-archieves-card");
+            cardToChange.addClass("second-archieves-card");
+        }
+        if(clickedIndex == 3){
+    
+            clickedCard.removeClass("third-archieves-card");
+            clickedCard.addClass("fifth-archieves-card");
+            cardToChange.removeClass("fifth-archieves-card");
+            cardToChange.addClass("third-archieves-card");
+        }
+        if(clickedIndex == 4){
+    
+            clickedCard.removeClass("fourth-archieves-card");
+            clickedCard.addClass("fifth-archieves-card");
+            cardToChange.removeClass("fifth-archieves-card");
+            cardToChange.addClass("fourth-archieves-card");
+        }
+    })  
+}
+// Modal Trigger on smartphone
+else{
 
-        clickedCard.removeClass("first-archieves-card");
-        clickedCard.addClass("fifth-archieves-card");
-        cardToChange.removeClass("fifth-archieves-card");
-        cardToChange.addClass("first-archieves-card");
-    }
-    if(clickedIndex == 2){
+    $("#archives-folder").click(() => {
 
-        clickedCard.removeClass("second-archieves-card");
-        clickedCard.addClass("fifth-archieves-card");
-        cardToChange.removeClass("fifth-archieves-card");
-        cardToChange.addClass("second-archieves-card");
-    }
-    if(clickedIndex == 3){
+        archievesModal.show();
+    });
+}
 
-        clickedCard.removeClass("third-archieves-card");
-        clickedCard.addClass("fifth-archieves-card");
-        cardToChange.removeClass("fifth-archieves-card");
-        cardToChange.addClass("third-archieves-card");
-    }
-    if(clickedIndex == 4){
-
-        clickedCard.removeClass("fourth-archieves-card");
-        clickedCard.addClass("fifth-archieves-card");
-        cardToChange.removeClass("fifth-archieves-card");
-        cardToChange.addClass("fourth-archieves-card");
-    }
-})
