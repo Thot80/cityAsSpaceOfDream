439a1e5296fb9f1b377f22d85016f5053eafad1b
Jeremy Kessous <jeremykessous@yahoo.fr>
2021-11-30 14:59:59 +0100

    linting indentation

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,22 +5,21 @@ let smartPhoneDevice;
 let oldDevice = false;
 
 // Desktop behaviour
-if (screenWidth >= 768){
+if (screenWidth >= 768) {
     smartPhoneDevice = false;
 }
 // Smartphone behaviour
-else if(screenWidth >= 576){
+else if (screenWidth >= 576) {
     smartPhoneDevice = true;
-}
-else{
+} else {
     smartPhoneDevice = true;
     oldDevice = true;
 }
 
 // ------------------- Change Folders'image size (source)  on smartphones -------------//
 
-if(smartPhoneDevice){
-    $("#research-folder").attr("src","assets/img/small-brown-folder.png")
+if (smartPhoneDevice) {
+    $("#research-folder").attr("src", "assets/img/small-brown-folder.png")
 }
 
 // ---------------- Archieves folder pop-up ------------------//
@@ -30,62 +29,61 @@ let isArchieveFolderOpen = false;
 
 // ------------------ Desktop behaviour --------------------------//
 
-if(smartPhoneDevice === false){
+if (smartPhoneDevice === false) {
 
     // Toggle Hidden archieves files when folder clicked
-    $("#archives-folder").click(function() {
+    $("#archives-folder").click(function () {
 
-        if($(".archieves-cards").hasClass("card-active")){
+        if ($(".archieves-cards").hasClass("card-active")) {
             isArchieveFolderOpen = true;
-        }
-        else{
+        } else {
             isArchieveFolderOpen = false;
         }
-        if(isArchieveFolderOpen === false){
-            $(".archieves-cards").toggleClass("card-active");  
+        if (isArchieveFolderOpen === false) {
+            $(".archieves-cards").toggleClass("card-active");
         }
     });
-    
+
 
     // Card changing position when clicked
-    $(".archieves-cards").click( function() {
-    
+    $(".archieves-cards").click(function () {
+
         // z-index value match the id of the car, z-index:n -> nth card
         let clickedIndex = $(this).css("z-index");
         let clickedCard = $(this);
         let cardToChange = $("div.fifth-archieves-card");
-        if(clickedIndex == 1){
-    
+        if (clickedIndex == 1) {
+
             clickedCard.removeClass("first-archieves-card");
             clickedCard.addClass("fifth-archieves-card");
             cardToChange.removeClass("fifth-archieves-card");
             cardToChange.addClass("first-archieves-card");
         }
-        if(clickedIndex == 2){
-    
+        if (clickedIndex == 2) {
+
             clickedCard.removeClass("second-archieves-card");
             clickedCard.addClass("fifth-archieves-card");
             cardToChange.removeClass("fifth-archieves-card");
             cardToChange.addClass("second-archieves-card");
         }
-        if(clickedIndex == 3){
-    
+        if (clickedIndex == 3) {
+
             clickedCard.removeClass("third-archieves-card");
             clickedCard.addClass("fifth-archieves-card");
             cardToChange.removeClass("fifth-archieves-card");
             cardToChange.addClass("third-archieves-card");
         }
-        if(clickedIndex == 4){
-    
+        if (clickedIndex == 4) {
+
             clickedCard.removeClass("fourth-archieves-card");
             clickedCard.addClass("fifth-archieves-card");
             cardToChange.removeClass("fifth-archieves-card");
             cardToChange.addClass("fourth-archieves-card");
         }
-    })  
+    })
 }
 // Modal Trigger on smartphone
-else{
+else {
 
     $("#archives-folder").click(() => {
         archievesModal.show();
@@ -97,14 +95,13 @@ let calendarModal = new bootstrap.Modal($("#calendarModal"));
 
 
 
-if (oldDevice == false){
-    $("#calendar").click(function(){
+if (oldDevice == false) {
+    $("#calendar").click(function () {
         $("#calendar-text").toggleClass("calendarTextDisabled");
     })
-}
-else{
+} else {
     calendarModal.show();
-    $("#calendar").click(function(){
+    $("#calendar").click(function () {
         calendarModal.show();
     });
 }
@@ -112,68 +109,67 @@ else{
 
 // ------------------------------------ research folder pop up -----------------------------------------//
 
- let researchModal = new bootstrap.Modal($("#researchModal1"));
+let researchModal = new bootstrap.Modal($("#researchModal1"));
 
 // ------------------ Desktop behaviour --------------------------//
 let isResearchFolderOpen = false;
 
-if(smartPhoneDevice === false){
+if (smartPhoneDevice === false) {
 
     // Toggle Hidden research files when folder clicked
-    $("#research-folder").click(function() {
+    $("#research-folder").click(function () {
 
-        if($(".research-cards").hasClass("card-active")){
+        if ($(".research-cards").hasClass("card-active")) {
             isResearchFolderOpen = true;
-        }
-        else{
+        } else {
             isResearchFolderOpen = false;
         }
-        if(isResearchFolderOpen === false){
-            $(".research-cards").toggleClass("card-active");  
+        if (isResearchFolderOpen === false) {
+            $(".research-cards").toggleClass("card-active");
         }
-    
+
     });
-    
+
 
     // Card changing position when clicked
-    $(".research-cards").click( function() {
-    
+    $(".research-cards").click(function () {
+
         // z-index value match the id of the car, z-index:n -> nth card
         let clickedIndex = $(this).css("z-index");
         let clickedCard = $(this);
         let cardToChange = $("div.third-research-card");
-        if(clickedIndex == 1){
-    
+        if (clickedIndex == 1) {
+
             clickedCard.removeClass("first-research-card");
             clickedCard.addClass("third-research-card");
             cardToChange.removeClass("third-research-card");
             cardToChange.addClass("first-research-card");
         }
-        if(clickedIndex == 2){
-    
+        if (clickedIndex == 2) {
+
             clickedCard.removeClass("second-research-card");
             clickedCard.addClass("third-research-card");
             cardToChange.removeClass("third-research-card");
             cardToChange.addClass("second-research-card");
         }
-        if(clickedIndex == 3){
-    
+        if (clickedIndex == 3) {
+
             clickedCard.removeClass("third-research-card");
             clickedCard.addClass("third-research-card");
             cardToChange.removeClass("fifth-research-card");
             cardToChange.addClass("third-research-card");
         }
-        if(clickedIndex == 4){
-    
+        if (clickedIndex == 4) {
+
             clickedCard.removeClass("fourth-research-card");
             clickedCard.addClass("third-research-card");
             cardToChange.removeClass("fifth-research-card");
             cardToChange.addClass("third-research-card");
         }
-    })  
+    })
 }
 // Modal Trigger on smartphone
-else{
+else {
     $("#research-folder").click(() => {
         researchModal.show();
     });
@@ -188,41 +184,39 @@ let xperimentModal = new bootstrap.Modal($("#xperimentsModal"));
 let isXperimentsFolderOpen = false;
 // ------------------ Desktop behaviour --------------------------//
 
-if(smartPhoneDevice === false){
+if (smartPhoneDevice === false) {
 
     // Toggle Hidden xperiment files when folder clicked
     $("#experiments-folder").click(() => {
-        if($(".xperiments-cards").hasClass("card-active")){
+        if ($(".xperiments-cards").hasClass("card-active")) {
             isXperimentsFolderOpen = true;
-        }
-        else{
+        } else {
             isXperimentsFolderOpen = false;
         }
-        if(isXperimentsFolderOpen === false){
-            $(".xperiments-cards").toggleClass("card-active");  
+        if (isXperimentsFolderOpen === false) {
+            $(".xperiments-cards").toggleClass("card-active");
         }
     });
 }
 // Modal Trigger on smartphone
-else{
+else {
     $("#experiments-folder").click(() => {
         xperimentModal.show();
     });
 }
 // -------------------------------------- Footer ---------------------------------------//
-if(oldDevice == false){
-    $("#partners-toggle").hover(function(){
+if (oldDevice == false) {
+    $("#partners-toggle").hover(function () {
         $(".partners-text").toggleClass("partners-text-enabled");
     });
-}
-else{
-    $("#partners-toggle").click(function(){
+} else {
+    $("#partners-toggle").click(function () {
         $(".partners-text").toggleClass("partners-text-enabled");
     });
 }
 
 // ------------------------------------------ Close buttons for cards ----------------------------------//
 
-$(".card-btn-close").click(function(){
+$(".card-btn-close").click(function () {
     $(this).parent().parent().toggleClass("card-active");
 });
